f56ad2b3998dd15a3fa09cdfdbe8051bb5230bdd
ngocnt <tanngocn@gmail.com>
2019-03-30 19:52:07 +0700

    New:change

--- dog.js
+++ dog.js
@@ -4,6 +4,6 @@ function Dog(name){
 	this.name=name;
 }
 Dog.prototype.sayHi=function(){
-	console.log('Hello, My name is',chalk.blue(this.name))
+	console.log('Hello, Your name is',chalk.blue(this.name))
 }
 module.exports =Dog
